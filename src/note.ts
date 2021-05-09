f6764ef4790c5a2a0a822867e56d601b4b1ca2c8
Joel Aday <alu0100987584@ull.edu.es>
2021-05-09 14:52:26 +0100

    code

--- src/note.ts
+++ src/note.ts
@@ -1,10 +1,4 @@
 
-/**
- * @enum colors Possible colors
- */
-
-
-
 export class Note {
 
   constructor(private user : string, private title : string, private body : string, private color : string) {}
@@ -12,24 +6,15 @@ export class Note {
   getName(): string {
     return this.user;
   }
-  /**
-   * Returns Note's title
-   * @returns Note's title
-   */
+
   getTitle(): string {
     return this.title;
   }
-  /**
-   * Returns Note's body
-   * @returns Note's body
-   */
+
   getBody(): string {
     return this.body;
   }
-  /**
-   * Returns Note's color
-   * @returns Note's color
-   */
+
   getColor(): string {
     return this.color;
   }
